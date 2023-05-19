e34e4a380504a21a3f9122df7d07facd49c5c9b6
anildalar <dollorinfotech@gmail.com>
2023-05-19 10:28:25 +0530

    new

---
+++ app/blog/[slug]/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Page({params:{slug}}) {
+  return (
+    <div>Slug - {slug}</div>
+  )
+}
---
+++ app/shop/[...slug]/page.js
@@ -0,0 +1,9 @@
+
+import React from 'react'
+
+export default function Page({params:slug}) {
+    console.log(slug)
+    return (
+        <div>page </div>
+    )
+}
